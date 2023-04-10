1339529f63f548e13db669f4ecd7cfd223b6d40e
Rosislav <rostislavnosovskij467@gmail.com>
2023-04-10 16:34:07 +0300

    Домашнее задание к шестому семинару

---
+++ task 41/Program.cs
@@ -0,0 +1,26 @@
+﻿/*** Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
+Console.Clear();
+Console.WriteLine("Введите некотрое количество чисел через пробел:");
+string st = Console.ReadLine();
+int [] GetArrayFromString (string st){
+    string[] num = st.Split(" ");
+    int[] res = new int[num.Length];
+    for (int i =0; i<num.Length; i++)
+    {
+        res[i] = int.Parse(num[i]);
+    }
+return res;
+}
+int metod (int[] res){
+    int result = 0;
+    for (int i =0; i<res.Length; i++){
+        if (res[i] >0)
+        {
+          result++;
+        }
+    }
+    return result;
+}
+Console.WriteLine($"{metod(GetArrayFromString(st))}");
---
+++ task 43/Program.cs
@@ -0,0 +1,18 @@
+﻿/*** Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) */
+Console.Clear();
+Console.WriteLine("Введите значение k1:");
+double k1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение b1:");
+double b1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение k2:");
+double k2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение b2:");
+double b2 = double.Parse(Console.ReadLine());
+double[] metod (double k1, double k2, double b1, double b2){
+    double[] result = new double [2];
+    result[0] = (b2 - b1)/(k1-k2);
+    result[1] = result[0]*k1 + b1;
+    return result;
+}
+Console.WriteLine($"[ {String.Join(";", metod(k1,k2,b1,b2))} ]");
